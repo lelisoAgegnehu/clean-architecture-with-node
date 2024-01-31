a40ca13bb5345333f0cc1c8105d17c7b08c8316f
leliso <lelisoagegneh491@gmail.com>
2024-01-31 10:15:37 +0300

    ✨ feat: user controller

---
+++ src/controllers/user/addUser.controller.js
@@ -0,0 +1,29 @@
+const { Response } = require("../../frameWorks/common");
+
+module.exports = (dependencies) => {
+  const {
+    useCases: {
+      user: { addUserUseCase },
+    },
+  } = dependencies;
+  return async (req, res, next) => {
+    try {
+      const { body = {} } = req;
+      const { id, name, lastName, gender, meta } = body;
+      const addUser = addUserUseCase(dependencies);
+
+      const response = await addUser.execute({
+        id,
+        name,
+        lastName,
+        gender,
+        meta,
+      });
+
+      res.json(new Response({ status: true, content: response }));
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+};
---
+++ src/controllers/user/updateUser.controller.js
@@ -0,0 +1,31 @@
+const { Response } = require("../../frameWorks/common");
+
+module.exports = (dependencies) => {
+  const {
+    useCases: {
+      user: { updateUserUseCase },
+    },
+  } = dependencies;
+  return async (req, res, next) => {
+    try {
+      const { body = {} } = req;
+      const { id, name, lastName, gender, meta } = body;
+      const updateUser = updateUserUseCase(dependencies);
+
+      const response = await updateUser.execute({
+        user: {
+          id,
+          name,
+          lastName,
+          gender,
+          meta,
+        },
+      });
+
+      res.json(new Response({ status: true, content: response }));
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+};
