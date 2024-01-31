eb099a2ae5d7288ad3b478469645b86b8df594fc
leliso <lelisoagegneh491@gmail.com>
2024-01-31 10:24:39 +0300

    export commons

---
+++ src/frameWorks/repositories/inMemory/index.js
@@ -0,0 +1,9 @@
+const userRepository = require("./users.repository");
+const productRepository = require("./products.repository");
+const orderRepository = require("./orders.repository");
+
+module.exports = {
+  userRepository,
+  productRepository,
+  orderRepository,
+};
